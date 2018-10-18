96e4b896800815cc88dfb3aefb3c1eb29dcebbb9
victorsmits <victor.smits@student.vinci.be>
2018-10-18 15:50:23 +0200

    first commit

---
+++ Dyck_Language/NUnitTestClass.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+namespace Dyck_Language
+{
+    [TestFixture()]
+    public class NUnitTestClass
+    {
+
+
+        [Test()]
+        public void TestIsDyckString()
+        {
+            var alphabet = new Dictionary<string, string>
+            {
+                { "(", ")" },
+                { "{", "}" },
+                { "[", "]" }
+            };
+
+            Assert.IsTrue(MainClass.IsDyckString("", alphabet));
+            Assert.IsTrue(MainClass.IsDyckString("bonjour (victor)", alphabet));
+            Assert.IsTrue(MainClass.IsDyckString("(())", alphabet));
+            Assert.IsTrue(MainClass.IsDyckString("{{}}", alphabet));
+            Assert.IsFalse(MainClass.IsDyckString(")(())", alphabet));
+            Assert.IsFalse(MainClass.IsDyckString("}(())", alphabet));
+            Assert.IsFalse(MainClass.IsDyckString("{(})", alphabet));
+        }
+    }
+}
---
+++ Dyck_Language/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace Dyck_Language
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+
+        }
+        /*
+        * @pre -
+        * @post The returned value contains true if ’s’ is a Dyck word
+        * given that the alphabet only contains ’(’ and ’)’
+        * and false otherwise
+        */
+        public static bool IsDyckString(string s, Dictionary<string, string> alphabet)
+        {
+            Stack pile = new Stack();
+            try
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    var elem = s[i].ToString();
+
+                    if (alphabet.ContainsKey(elem))
+                    {
+                        pile.Push(s[i]);
+                    }
+                    else if (alphabet.ContainsValue(elem))
+                    {
+                        var last = pile.Peek().ToString();
+
+                        if (s[i].ToString() == alphabet[last])
+                        {
+                            pile.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return pile.Count == 0;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+        }
+    }
+
+}
